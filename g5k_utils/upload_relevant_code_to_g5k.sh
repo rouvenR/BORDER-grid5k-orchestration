d4e8dcfb715083366727a95ed11e6234829318bc
Rouven Anderer <rouven.anderer@siemens.com>
2026-06-29 13:02:21 +0200

    fix: relative path

--- g5k_utils/upload_relevant_code_to_g5k.sh
+++ g5k_utils/upload_relevant_code_to_g5k.sh
@@ -4,44 +4,46 @@ set -euo pipefail
 
 pids=()
 
-scp ../BORDER/launch_experiments.sh randerer@access.grid5000.fr:grenoble/launch_experiments.sh &
-pids+=("$!")
-scp ../BORDER/flexible_router.py randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/flexible_router.py &
-pids+=("$!")
-scp ../BORDER/start_clients.sh randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/start_clients.sh &
-pids+=("$!")
-scp ../BORDER/clients/alpine_container/build.sh randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/clients/alpine_container/build.sh &
-pids+=("$!")
-scp ../BORDER/border_setup.sh randerer@access.grid5000.fr:grenoble/border_setup.sh &
-pids+=("$!")
-scp ../BORDER/border_setup_launch.sh randerer@access.grid5000.fr:grenoble/border_setup_launch.sh &
-pids+=("$!")
-scp ../BORDER/launch_border_via_ssh.sh randerer@access.grid5000.fr:grenoble/launch_border_via_ssh.sh &
-pids+=("$!")
-scp ../BORDER/clients/alpine_container/sub_thread.py randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/clients/alpine_container/sub_thread.py &
-pids+=("$!")
-scp ../jorammq-deployment/Dockerfile randerer@access.grid5000.fr:grenoble/jorammq-deployment/Dockerfile &
-pids+=("$!")
-scp ../jorammq-deployment/build.sh randerer@access.grid5000.fr:grenoble/jorammq-deployment/build.sh &
-pids+=("$!")
-scp ../jorammq-deployment/start.sh randerer@access.grid5000.fr:grenoble/jorammq-deployment/start.sh &
-pids+=("$!")
-scp ../jorammq-deployment/entrypoint.sh randerer@access.grid5000.fr:grenoble/jorammq-deployment/entrypoint.sh &
-pids+=("$!")
-scp ../mzbench-docker-deployment/Dockerfile randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/Dockerfile &
-pids+=("$!")
-scp ../mzbench-docker-deployment/server.config randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/server.config &
-pids+=("$!")
-scp ../mzbench-docker-deployment/build.sh randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/build.sh &
-pids+=("$!")
-scp ../mzbench-docker-deployment/run.sh randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/run.sh &
-pids+=("$!")
-scp ./automated_data_pipeline.sh randerer@access.grid5000.fr:grenoble/automated_data_pipeline.sh &
-pids+=("$!")
-scp -r  ../border-data-pipeline/*.py randerer@access.grid5000.fr:grenoble/border-data-pipeline/
-scp data_pipeline.sh randerer@access.grid5000.fr:grenoble/data_pipeline.sh
+PROJECT_ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# scp ../jorammq-deployment/jorammq-mqtt-trial-1.22.0-SNAPSHOT.zip randerer@access.grid5000.fr:grenoble/jorammq-deployment/jorammq-mqtt-trial-1.22.0-SNAPSHOT.zip
+scp "$PROJECT_ROOT_DIR/BORDER/launch_experiments.sh" randerer@access.grid5000.fr:grenoble/launch_experiments.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/flexible_router.py" randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/flexible_router.py &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/start_clients.sh" randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/start_clients.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/clients/alpine_container/build.sh" randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/clients/alpine_container/build.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/border_setup.sh" randerer@access.grid5000.fr:grenoble/border_setup.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/border_setup_launch.sh" randerer@access.grid5000.fr:grenoble/border_setup_launch.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/launch_border_via_ssh.sh" randerer@access.grid5000.fr:grenoble/launch_border_via_ssh.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/BORDER/clients/alpine_container/sub_thread.py" randerer@access.grid5000.fr:grenoble/border/containernet/BORDER/clients/alpine_container/sub_thread.py &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/jorammq-deployment/Dockerfile" randerer@access.grid5000.fr:grenoble/jorammq-deployment/Dockerfile &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/jorammq-deployment/build.sh" randerer@access.grid5000.fr:grenoble/jorammq-deployment/build.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/jorammq-deployment/start.sh" randerer@access.grid5000.fr:grenoble/jorammq-deployment/start.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/jorammq-deployment/entrypoint.sh" randerer@access.grid5000.fr:grenoble/jorammq-deployment/entrypoint.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/mzbench-docker-deployment/Dockerfile" randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/Dockerfile &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/mzbench-docker-deployment/server.config" randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/server.config &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/mzbench-docker-deployment/build.sh" randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/build.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/mzbench-docker-deployment/run.sh" randerer@access.grid5000.fr:grenoble/mzbench-docker-deployment/run.sh &
+pids+=("$!")
+scp "$PROJECT_ROOT_DIR/automated_data_pipeline.sh" randerer@access.grid5000.fr:grenoble/automated_data_pipeline.sh &
+pids+=("$!")
+scp -r "$PROJECT_ROOT_DIR/border-data-pipeline/"*.py randerer@access.grid5000.fr:grenoble/border-data-pipeline/
+scp "$PROJECT_ROOT_DIR/data_pipeline.sh" randerer@access.grid5000.fr:grenoble/data_pipeline.sh
+
+# scp "$PROJECT_ROOT_DIR/jorammq-deployment/jorammq-mqtt-trial-1.22.0-SNAPSHOT.zip" randerer@access.grid5000.fr:grenoble/jorammq-deployment/jorammq-mqtt-trial-1.22.0-SNAPSHOT.zip
 
 
 status=0
