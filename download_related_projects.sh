da5fb3205dcc7b3df9ed1eefa1ef654a82ce29d0
Rouven Anderer <rouven.anderer@siemens.com>
2026-07-07 10:28:44 +0200

    fix: git path

--- download_related_projects.sh
+++ download_related_projects.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone git@github.com:rouvenR/BORDER.git && cd BORDER && git checkout g5k-launch && cd ..
-git clone https://github.com/rouvenR/jorammq-deployment && cd jorammq-deployment && git checkout final-state-thesis && cd ..
+git clone git@github.com:rouvenR/jorammq-deployment.git && cd jorammq-deployment && git checkout final-state-thesis && cd ..
 # JoramMQ zip has to be added to ./jorammq-deployment/jorammq-mqtt-trial-1.22.0-SNAPSHOT.zip
 
 git clone git@github.com:rouvenR/mzbench-docker-deployment.git && cd mzbench-docker-deployment && git checkout final-state-thesis && cd ..
